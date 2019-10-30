bfdd107308fe922b738baf6fcd49504bc9f0e53b
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-30 21:09:37 +0100

    no warnings

--- init.el
+++ init.el
@@ -24,7 +24,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-(setq package-enable-at-startup nil)
+;;(setq package-enable-at-startup nil)
 (package-initialize)
 
 ;; workaround bug in Emacs 26.2
